08080f0d154f58aecf172c575a593fd9b9e6e95b
ostrichbean <29381649+ostrichbean@users.noreply.github.com>
2018-05-07 14:32:56 +0100

    Neat and working to previous standard
    now need to sort out speeds and drills

--- output/gcode/create_gcode.py
+++ output/gcode/create_gcode.py
@@ -7,54 +7,63 @@ class gcode_block:
                 self.data = (self.data).replace("F", "G00 F")   #replace feedrate commands with ones Marlin understands
         def delete_ends(self):
                 self.data = (self.data).replace("G00 X0Y0\nM05", ";G00 X0Y0; delete_ends\r\n;M05; delete_ends")
-        def read_from_disk(self, read_file_name, n_iterations):
+        def read_from_disk(self, read_file_name, n_iterations = 1):
                 with open(read_file_name, "r") as read_file:
                         read_data = read_file.read()
                         for i in range(n_iterations):   
                                 self.data += read_data
+        def read_from_gcode_block(self, read_block, n_iterations = 1):
+                self.data += read_block.data
         def save_to_disk(self, save_file_name):
                 with open(save_file_name, "w+") as save_file:
                         save_file.write(self.data)
 
 
-#main
-def main():
+#setups
+
+def create_paint(output_name):
+        
+        #paint
         paint = gcode_block("")
-        #files to collect
-        paint.read_from_disk("start.gcode", 1)
-        paint.read_from_disk("F.Cu.gbr_iso_cnc.gcode", 1)
-        paint.read_from_disk("paint.write_file_to_file", 1)
-        paint.read_from_disk("end.gcode", 1)
+
+        paint.read_from_disk("start.gcode")
+        paint.read_from_disk("F.Cu.gbr_iso_cnc.gcode")
+        paint.read_from_disk("drl_cnc.gcode")
+        paint.read_from_disk("end.gcode")
 
         paint.fixes()
         paint.delete_ends()
-        paint.save_to_disk("PCB_paint.gcode")
+        paint.save_to_disk(output_name)
 
+def create_cutting(output_name):
         
-        #modify operations
-
-        #collect operations
-        paint = setup("PCB_paint.gcode")
-        paint.append(FCu, 1)
-        paint.append(drl_mill, 1)
-        paint.append(cutout, 1)
-        #apply modifications and write to file
-        paint.finish(start, end)
-
-        drl_mill_1 = operation("ESP pogo programmer.drl_mill_cnc_1.gcode")
-        cutout_1 = operation("ESP pogo programmer-F.Cu.gbr_cutout_cnc_1.gcode")
+        #cutting_body
+        cutting_body = gcode_block("")
         
-        cutting = setup("PCB_cutting.gcode")
-        #cutting.append(drl_mill_1, 1)
-        cutting.append(cutout_1, 1)
+        cutting_body.read_from_disk("drl_cnc.gcode")
+        #cutting_body.read_from_disk("edge cuts.gcode")
+        
+        cutting_body.fixes()
+        cutting_body.delete_ends()
+        
+        #cutting
+        cutting = gcode_block("")
+        
+        cutting.read_from_disk("start.gcode")
+        cutting.read_from_gcode_block(cutting_body, 500)
+        cutting.read_from_disk("end.gcode")
 
-        cutting.fixes()
-        cutting.delete_ends()
-        (cutting.file).write(start.data)
-        for i in range(500):
-                cutting.write_to_file()
-        (cutting.file).write(end.data)
+        cutting.save_to_disk(output_name)
+
+
+#main
+
+def main():
+        create_paint("PCB_paint.gcode")
+        create_cutting("PCB_cutting.gcode")        
+
 
 #run
+
 main()
 
